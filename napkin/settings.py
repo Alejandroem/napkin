69d3e63ced0240898e53a3f9883e653fbafff3e9
alejandroem <alexalejandroem@gmail.com>
2021-08-05 00:14:23 +0200

    Changed static files root

--- napkin/settings.py
+++ napkin/settings.py
@@ -144,8 +144,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = 'var/static_root/'
+STATIC_URL = './static/'
+STATIC_ROOT = './var/static_root/'
 STATICFILES_DIRS = ['static']
 
 # Default primary key field type

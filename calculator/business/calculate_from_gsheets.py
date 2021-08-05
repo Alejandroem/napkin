7f4a680bfff3aa2d4f8cd55f61bc1f3339a52498
alejandroem <alexalejandroem@gmail.com>
2021-08-05 15:37:21 +0200

    Removed comment

--- calculator/business/calculate_from_gsheets.py
+++ calculator/business/calculate_from_gsheets.py
@@ -78,9 +78,9 @@ def calculate_from_gsheets(values):
         'yieldOnCost': resultsSheet.acell('B5').value,
     }
 
-    # try:
-    #     created_file = service.files().delete(fileId=created_file['id']).execute()
-    # except errors.HttpError as error:
-    #     print(error)
-    #     raise ValueError('An error occured while deleting the file' + error.error_details)
+    try:
+        created_file = service.files().delete(fileId=created_file['id']).execute()
+    except errors.HttpError as error:
+        print(error)
+        raise ValueError('An error occured while deleting the file' + error.error_details)
     return response
